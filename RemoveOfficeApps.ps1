a7d45041c8fd7b71aaf0cf00db92ef9667ee7893
Akash Chawla <akashchawla@microsoft.com>
2022-04-27 18:14:15 -0700

    changes

--- RemoveOfficeApps.ps1
+++ RemoveOfficeApps.ps1
@@ -1,184 +1,257 @@
-<#
-    Install Office 365 via ODT. Script to be used by Flexxible IT Apps2Digital formulas
-
-    https://docs.microsoft.com/en-us/deployoffice/overview-of-the-office-2016-deployment-tool
-
-    @guyrleech 2019
+<#Author       : Akash Chawla
+# Usage        : RDP Shortpath
 #>
 
-[CmdletBinding()]
+#############################################
+#         Remove Office applications        #
+#############################################
 
 
-$ProgressPreference = 'SilentlyContinue'
 
-## TODO Replace "64" with a #Bitness# parameter
-## TODO Option to install Visio
-## TODO Option to not install OneDrive
+[CmdletBinding()] Param (
+    [Parameter(
+        Mandatory
+    )]
+    [ValidateSet("Word","PowerPoint","Access","Excel","OneNote","Outlook","Publisher","Neptune","Visio", "Publisher", "Skype")]
+    [System.String[]]$Applications,
 
-#SourcePath="**officeDownloadFolder**" 
-[string]$configXML = @'
-<Configuration>
-  <Add OfficeClientEdition="64" Channel="Current">
-    <Product ID="O365ProPlusRetail">
-        <Language ID="MatchOS" />
-        <ExcludeApp ID="Groove" />
-        <ExcludeApp ID="Lync" />
-        <ExcludeApp ID="Word" />
-        <ExcludeApp ID="PowerPoint" />
-        <ExcludeApp ID="Teams" />
-    </Product>
-  </Add>
-    <Updates Enabled="FALSE" Channel="Monthly" />
-    <Display Level="None" AcceptEULA="TRUE" />
-    <Property Name="AUTOACTIVATE" Value="0" />
-    <Logging Level="Standard" Path="**workingFolder**" />
-</Configuration>
+    [Parameter(
+        Mandatory
+    )]
+    [string]$Version,
+
+    [Parameter(
+        Mandatory
+    )]
+    [string]$Type
+
+)
+
+function AddProductsToConfigurationXML($Applications, $xmlFile, $xmlFilePath, $Version) {
+    Begin {
+
+        try {
+            $addElement = $xmlFile.DocumentElement.Add
+
+            if ($null -eq $addElement) {
+                Throw "Not able to access the xml element"
+            }
+
+
+            $addElement.setAttribute("OfficeClientEdition", $Version)
+            $VisioProductID = "VisioProRetail"
+            $projectProductID = "ProjectProRetail"
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        }
+    }
+
+    Process {
+
+        try {
+            foreach ($app in $Applications) {
+
+                $productElement = $xmlFile.CreateElement("Product");
+                $languageElement = $xmlFile.CreateElement("Language")
+                $languageElement.setAttribute("ID", "MatchOS")
+    
+                $productElement.AppendChild($languageElement)
+    
+    
+                if ($app -eq "Visio") {
+                    $productElement.setAttribute("ID", $VisioProductID)
+                }
+    
+                if ($app -eq "Project") {
+                    $productElement.setAttribute("ID", $ProjectProductID)
+                }
+    
+                $addElement.AppendChild($productElement)
+            }
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        }
+    }
+
+    End {
+
+        $xmlFile.Save($xmlFilePath)
+    }
+}
+
+function RemoveProductsFromConfigurationXML($Applications, $xmlFile, $xmlFilePath, $Version) {
+
+    Begin {
+
+        try {
+            $addElement = $xmlFile.DocumentElement.Add
+            $addElement.setAttribute("OfficeClientEdition", $Version)
+            $productID = "O365ProPlusRetail"
+            $productElement = $xmlFile.CreateElement("Product")
+            $productElement.setAttribute("ID", $productID)
+
+            $languageElement = $xmlFile.CreateElement("Language")
+            $languageElement.setAttribute("ID", "MatchOS")
+
+            $productElement.AppendChild($languageElement)
+        } 
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        }
+    }
+
+    Process {
+
+        try {
+            foreach ($app in $Applications) {
+
+                $excludeElement = $xmlFile.CreateElement("ExcludeApp")
+                $excludeElement.setAttribute("ID", $app);
+                $productElement.AppendChild($excludeElement)
+            }
+    
+            $addElement.AppendChild($productElement)
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        }
+    }
+
+    End {
+
+        $xmlFile.Save($xmlFilePath)
+    }
+}
+
+function ConfigureOfficeXML($Applications, $xmlFile, $xmlFilePath, $Version, $Type) {
+
+    if ($Type -eq "Add") {
+        Write-Host " AVD AIB Customization Office apps: Adding office applications"
+        AddProductsToConfigurationXML -Applications $Applications -xmlFile $file -xmlFilePath $xmlFilePath -Version $Version
+    } 
+    else {
+        Write-Host " AVD AIB Customization Office apps: Removing office applications"
+        RemoveProductsFromConfigurationXML -Applications $Applications -xmlFile $file -xmlFilePath $xmlFilePath -Version $Version
+    }
+}
+
+function installOfficeUsingODT($Applications, $Version, $Type) {
+
+
+    Begin {
+
+        $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+        Write-Host "Starting AVD AIB Customization : Office Apps : $((Get-Date).ToUniversalTime())"
+
+        $configXML = @'
+            <Configuration>
+            <Add Channel="Monthly">
+            </Add>
+            <RemoveMSI />
+            <Updates Enabled="FALSE" />
+            <Display Level="None" AcceptEULA="TRUE" />
+            <Property Name="FORCEAPPSHUTDOWN" Value="TRUE" />
+            <Property Name="SharedComputerLicensing" Value="1" />
+            </Configuration>
 '@
 
-[bool]$createdFolder = $false
-[string]$odtDownloadRegex = '/officedeploymenttool[a-z0-9_-]*\.exe$'
+        $ODTDownloadLinkRegex = '/officedeploymenttool[a-z0-9_-]*\.exe$'
+        $guid = [guid]::NewGuid().Guid
+        $tempFolder = (Join-Path -Path "C:\temp\" -ChildPath $guid)
+        $ODTDownloadUrl = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
 
-## Check running as admin
-# $myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
-# $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-# if ( ! ( $myWindowsPrincipal.IsInRole( $adminRole ) ) )
-# {
-#     Throw 'Script is not being run elevated'
-# }
+        if (!(Test-Path -Path $tempFolder)) {
+            New-Item -Path $tempFolder -ItemType Directory
+        }
 
-try
-{
-    [string]$workingFolder = (Join-Path -Path $env:temp -ChildPath (([guid]::NewGuid()).Guid))
-
-    # URL the ODT
-    [string]$downloadURL = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
-
-    if( ( Test-Path -Path $workingFolder -ErrorAction SilentlyContinue ) )
-    {
-        Throw "Random working folder name `"$workingFolder`" already exists"
+        Write-Host "AVD AIB Customization Office Apps : Created temp folder $tempFolder"
     }
 
-    $newFolder = New-Item -Path $workingFolder -ItemType Directory
-    if( ! $newFolder )
-    {
-        Throw "Failed to create working folder `"$workingFolder`""
-    }
-    else
-    {
-        $createdFolder = $true
-    }
-    
-    $html = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
-    
-    if( ! $html -or ! $html.Links )
-    {
-        Throw "Failed to download from $downloadURL"
-    }
+    Process {
 
-    [string[]]$downloadLinks = @( $html.Links | Where-Object { $_.href -Match $odtDownloadRegex } | Select-Object -ExpandProperty href | Sort-Object -Unique )
+        try {
+         
+            $HttpContent = Invoke-WebRequest -Uri $ODTDownloadUrl -UseBasicParsing
+            
+            if ($HttpContent.StatusCode -ne 200) { 
+                throw "Office Installation script failed to find Office deployment tool link -- Response $($Response.StatusCode) ($($Response.StatusDescription))"
+            }
 
-    if( ! $downloadLinks.Count )
-    {
-        Throw "No links on page $downloadURL matching regex $odtDownloadRegex"
-    }
+            $ODTDownloadLinks = $HttpContent.Links | Where-Object { $_.href -Match $ODTDownloadLinkRegex }
 
-    if( $downloadLinks.Count -gt 1 )
-    {
-        Throw "Ambiguous download links on $downloadURL : $($downloadLinks -join ',')"
-    }
+            #pick the first link in case there are multiple
+            $ODTToolLink = $ODTDownloadLinks[0].href
+            Write-Host "AVD AIB Customization Office Apps : Office deployment tool link is $ODTToolLink"
 
-    [string]$outputFile = Join-Path -Path $workingFolder -ChildPath ($downloadLinks[0] -split '/')[-1]
+            $ODTexePath = Join-Path -Path $tempFolder -ChildPath "officedeploymenttool.exe"
 
-    Write-Verbose "$(Get-Date -Format G): Starting download from $($downloadLinks[0]) to `"$outputFile`""
+            #download office deployment tool
 
-    (New-Object System.Net.WebClient).Downloadfile( $downloadLinks[0] , $outputFile )
+            Write-Host "AVD AIB Customization Office Apps : Downloading ODT tool into folder $ODTexePath"
+            $ODTResponse = Invoke-WebRequest -Uri "$ODTToolLink" -UseBasicParsing -UseDefaultCredentials -OutFile $ODTexePath -PassThru
 
-    if( ! ( Test-Path -Path $outputFile -PathType Leaf -ErrorAction SilentlyContinue ) )
-    {
-        Throw "Failed to download from $($downloadLinks[0]) to `"$outputFile`""
-    }
+            if ($ODTResponse.StatusCode -ne 200) { 
+                throw "Office Installation script failed to download Office deployment tool -- Response $($ODTResponse.StatusCode) ($($ODTResponse.StatusDescription))"
+            }
 
-    # Now check signature to ensure we are running the correct executable
-    Unblock-File -Path $outputFile -ErrorAction SilentlyContinue
+            Write-Host "AVD AIB Customization Office Apps : Extracting setup.exe into $tempFolder"
+            # extract setup.exe
+            Start-Process -FilePath $ODTexePath -ArgumentList "/extract:`"$($tempFolder)`" /quiet" -PassThru -Wait -NoNewWindow
 
-    $signing = Get-AuthenticodeSignature -FilePath $outputFile -ErrorAction SilentlyContinue
-    if( ! $signing )
-    {
-        Throw "Could not get signing information from `"$outputFile`""
-    }
-    if( $signing.Status -ne 'Valid' )
-    {
-        Throw "Certificate status for `"$outputFile`" is $($signing.Status), not `"Valid`""
-    }
-    if( $signing.SignerCertificate.Subject -notmatch '^CN=Microsoft Corporation,' )
-    {
-        Throw "`"$outputFile`" is not signed by Microsoft Corporation, found $($signing.SignerCertificate.Subject)"
-    }
+            $setupExePath = Join-Path -Path $tempFolder -ChildPath 'setup.exe'
+            
+            # Construct XML config file for Office Deployment Kit setup.exe
+            $xmlFilePath = Join-Path -Path $tempFolder -ChildPath 'installOffice.xml'
 
-    # Run silently to extract setup.exe
-    $odtInstallProcess = Start-Process -FilePath $outputFile -ArgumentList "/extract:`"$($workingFolder)`" /quiet" -PassThru -Wait -WindowStyle Hidden
-    if( ! $odtInstallProcess )
-    {
-        Throw "Failed to run `"$outputFile`""
-    }
-    if( ! $odtInstallProcess -or $odtInstallProcess.ExitCode )
-    {
-        Throw "Bad exit code $($odtInstallProcess.ExitCode) from `"$outputFile`""
-    }
-    [string]$setupExe = Join-Path -Path $workingFolder -ChildPath 'setup.exe'
-    if( ! ( Test-Path -Path $setupExe -PathType Leaf -ErrorAction SilentlyContinue ) )
-    {
-        Throw "Unable to locate extracted setup.exe after running `"$outputFile`""
-    }
-    Unblock-File -Path $setupExe -ErrorAction SilentlyContinue
+            Write-Host "AVD AIB Customization Office Apps : Saving xml content into xml file : $xmlFilePath"
+            $configXML | Out-File -FilePath $xmlFilePath -Force -Encoding ascii
+            
+            [XML]$file = Get-Content $xmlFilePath
+            ConfigureOfficeXML -Applications $Applications -xmlFile $file -xmlFilePath $xmlFilePath -Version $Version -Type $Type
+            
+            Write-Host "AVD AIB Customization Office Apps : Running setup.exe to download Office"
+            $ODTRunSetupExe = Start-Process -FilePath $setupExePath -ArgumentList "/download $(Split-Path -Path $xmlFilePath -Leaf)" -PassThru -Wait -WorkingDirectory $tempFolder -WindowStyle Hidden
 
-    # Construct XML config file for Office Deployment Kit setup.exe
-    [string]$xmlFilePath = Join-Path -Path $workingFolder -ChildPath 'office365.xml'
-    ## TODO verify bitness is 32 or 64 bit
+            if (!$ODTRunSetupExe) {
+                Throw "AVD AIB Customization Office Apps : Failed to run `"$setupExePath`" to download Office"
+            }
 
-    # Get the log file in our folder. 
-    $configXML -replace '\*\*workingFolder\*\*' , $workingFolder | Out-File -FilePath $xmlFilePath -Force -Encoding ascii
-    # Run silently to download media
-    
-    Write-Verbose "$(Get-Date -Format G): Running `"$setupExe`" to download office media"
-    $odtProcess = Start-Process -FilePath $setupExe -ArgumentList "/download $(Split-Path -Path $xmlFilePath -Leaf)" -PassThru -Wait -WorkingDirectory $workingFolder -WindowStyle Hidden
+            if ( $ODTRunSetupExe.ExitCode) {
+                Throw "AVD AIB Customization Office Apps : Exit code $($ODTRunSetupExe.ExitCode) returned from `"$setupExePath`" to download Office"
+            }
 
-    if( ! $odtProcess )
-    {
-        Throw "Failed to run `"$setupExe`" to download Office"
+            Write-Host "AVD AIB Customization Office Apps : Running setup.exe to Install Office"
+            $InstallOffice = Start-Process -FilePath $setupExePath -ArgumentList "/configure $(Split-Path -Path $xmlFilePath -Leaf)" -PassThru -Wait -WorkingDirectory $tempFolder -WindowStyle Hidden
+
+            if (!$InstallOffice) {
+                Throw "AVD AIB Customization Office Apps : Failed to run `"$setupExePath`" to install Office"
+            }
+
+            if ( $ODTRunSetupExe.ExitCode ) {
+                Throw "AVD AIB Customization Office Apps : Exit code $($ODTRunSetupExe.ExitCode) returned from `"$setupExePath`" to download Office"
+            }
+            
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSitem)
+        }
     }
 
-    if( $odtProcess.ExitCode )
-    {
-        Throw "Bad exit code $($odtProcess.ExitCode) from `"$setupExe`" to download Office"
-    }
+    End {
 
-    Write-Verbose -Message "$(Get-Date -Format G): download of Office media to `"$workingFolder`" finished, starting installation"
+        #Cleanup
+        if ((Test-Path -Path $tempFolder -ErrorAction SilentlyContinue)) {
+            Remove-Item -Path $tempFolder -Force -Recurse -ErrorAction Continue
+        }
 
-    ## Install
-    $installProcess = Start-Process -FilePath $setupExe -ArgumentList "/configure $(Split-Path -Path $xmlFilePath -Leaf)" -PassThru -Wait -WorkingDirectory $workingFolder -WindowStyle Hidden
+        $stopwatch.Stop()
+        $elapsedTime = $stopwatch.Elapsed
+        Write-Host "Ending AVD AIB Customization : Office Apps - Time taken: $elapsedTime"
 
-    if( ! $installProcess )
-    {
-        Throw "Failed to run `"$setupExe`" to install Office"
-    }
-
-    if( $odtProcess.ExitCode )
-    {
-        Throw "Bad exit code $($odtProcess.ExitCode) from `"$setupExe`" to install Office"
-    }
-    
-    Write-Verbose -Message "$(Get-Date -Format G): installation of Office media from `"$workingFolder`" finished"
-}
-catch
-{
-    Throw $_
-}
-finally
-{
-    # Delete temp folder and files
-    if( ( Get-Variable -Name createdFolder -ErrorAction SilentlyContinue ) -and $createdFolder -and ( Test-Path -Path $workingFolder -ErrorAction SilentlyContinue ) )
-    {
-       Remove-Item -Path $workingFolder -Force -Recurse -ErrorAction Continue
     }
 }
+
+installOfficeUsingODT -Applications $Applications -Version $Version -Type $Type
+
