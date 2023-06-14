5d269e8282ab1190fdf3ce0db137769044cd3b32
Akash Chawla <akashchawla@microsoft.com>
2023-06-14 08:26:46 -0700

    sleep

--- Sleep.ps1
+++ Sleep.ps1
@@ -1,3 +1,3 @@
 Write-Host "*** AVD AIB CUSTOMIZER PHASE: Sleep started"
 Start-Sleep -Seconds "360000"
-Write-Host "*** AVD AIB CUSTOMIZER PHASE: Sleep ended"
+Write-Host "*** AVD AIB CUSTOMIZER PHASE: Sleep end"
