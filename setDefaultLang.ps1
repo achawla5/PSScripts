a5b56bd9870ae9dde109c903b0d991ef7254ab56
Akash Chawla <akashchawla@microsoft.com>
2023-08-21 20:45:48 -0700

    change

--- setDefaultLang.ps1
+++ setDefaultLang.ps1
@@ -103,46 +103,7 @@ try {
     $languageTag = $languageDetails[0]
     $GeoID = $languageDetails[1]
   }
-
-  $foundLanguage = $false;
-
-  try {
-    #install language pack in case the provided language is not installed
-    $installedLanguages = Get-InstalledLanguage
-    foreach($languagePack in $installedLanguages) {
-      $languageID = $languagePack.LanguageId
-      if($languageID -eq $LanguageTag) {
-        $foundLanguage = $true
-        break
-      }
-    } 
-  }
-  catch {
-    Write-Host "*** AVD AIB CUSTOMIZER PHASE: Set default Language - Exception occurred while installing language packs***"
-    Write-Host $PSItem.Exception
-  }
-
-  if(-Not $foundLanguage) {
-    # retry in case we hit transient errors
-    for($i=1; $i -le 5; $i++) {
-        try {
-            Write-Host "*** AVD AIB CUSTOMIZER PHASE : Set default language - Install language packs -  Attempt: $i ***"   
-            Install-Language -Language $LanguageTag
-            Write-Host "*** AVD AIB CUSTOMIZER PHASE : Set default lanhguage - Install language packs -  Installed language $LanguageCode ***"   
-            break
-        }
-        catch {
-            Write-Host "*** AVD AIB CUSTOMIZER PHASE : Set default language - Install language packs - Exception occurred***"
-            Write-Host $PSItem.Exception
-            continue
-        }
-    }
-  }
-  else {
-     Write-Host "*** AVD AIB CUSTOMIZER PHASE : Set default language - Language pack for $LanguageTag is installed already***"
-  }
-
-  #Install-Language -Language $LanguageTag -CopyToSettings 
+  
   Set-systempreferreduilanguage -Language $LanguageTag
   Set-WinSystemLocale -SystemLocale $LanguageTag
   Set-Culture -CultureInfo $LanguageTag
